fde919af4c8ee15f5b258e59e57da894a05ddc44
unknown <aniket83839@gmail.com>
2023-01-16 00:23:18 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('dark');
+})
 const header = document.querySelector('.Header');
 const nav = document.querySelector('.Navbar')
 const topOfNav = header.offsetTop;
